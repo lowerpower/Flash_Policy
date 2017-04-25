af72554ad644d5ab8d86274eccd7bc30cb38f6ff
lowerpower <lowerpower@users.noreply.github.com>
2017-04-24 20:18:45 -0700

    fixed installer files for ubuntu 16.04

--- sample_files/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-install -m644 policy.txt /etc/flash/ 
-install -m644 pconfig.txt /etc/flash/ 
-
-install -m755 ../src/flash_policyd /usr/local/bin/
-install -m755 flash_policyd /etc/rc.d/init.d/flash_policyd
-chkconfig --add flash_policyd
-
